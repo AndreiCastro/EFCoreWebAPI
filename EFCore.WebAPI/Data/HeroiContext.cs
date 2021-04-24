58cf91384f7170710691e47996a291f5e8a1149d
AndreiCastro <andrei.castro1991@gmail.com>
2021-04-24 15:24:57 -0300

    Reiniciando projeto

---
+++ EFCore.WebAPI/Data/HeroiContext.cs
@@ -0,0 +1,24 @@
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI.Data
+{
+    public class HeroiContext : DbContext
+    {
+        //Aqui é onde o migration ira criar as tabelas, cada atributo a baixo representa uma tabela.
+        //Add-Migration initial (criar a migration)
+        //Remove-Migration      (remover a dita cuja)
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Heroi> Herois { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=HeroApp;Data Source=ANDREI\SQLEXPRESS");
+        }
+    }
+}
---
+++ EFCore.WebAPI/Models/Arma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI.Models
+{
+    public class Arma
+    {
+        //Relaçao de 1x1
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Heroi Heroi { get; set; }
+        public int HeroiId { get; set; }
+    }
+}
